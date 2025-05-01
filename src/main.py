751840afe7e2e7a3a193cb9947096b257485f434
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-05-01 19:57:32 +0700

    this is what i do

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
-import fastapi
-from dotenv import loadenv
+from fastapi import FastAPI
+from supabase import create_client, Client
+from pydantic import BaseModel
+from dotenv import load_dotenv
 import os
+
+load_dotenv() 
+
+SUPABASE_URL = os.getenv("SUPABASEURL")
+SUPABASE_KEY = os.getenv("SUPABASEKEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+
+@app.get("/fetch")
+def fetch():
+    return "yes is working stop"
+
+@app.get("/usered")
+def get_usered():
+    response = supabase.table('usered').select('*').execute()
+    return response.data
