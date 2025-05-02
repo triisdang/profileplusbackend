68ff5cd9a4564a548ce72b8b6ef1b89c17d5494b
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-05-02 17:50:03 +0700

    /postimage

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from supabase import create_client, Client
@@ -6,14 +6,13 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 
-load_dotenv() 
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASEURL")
 SUPABASE_KEY = os.getenv("SUPABASEKEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 app = FastAPI()
-
 
 class User(BaseModel):
     name: str
@@ -26,6 +25,15 @@ def favicon():
 def test():
     response = supabase.table("test").select("*").execute()
     return response.data
-@app.get("/postimage")
-def postimage():
-    return "wip"
+
+@app.post("/postimage")
+def postimage(url: str = Header(...), username: str = Header(...)):
+    response = supabase.table('pendingDB').insert({
+        "created_by": username,
+        "url": url
+    }).execute()
+
+    if response.data:
+        return {"message": f"success, db: {response.data}"}
+    else:
+        return {"message": "Failed to insert data"}
